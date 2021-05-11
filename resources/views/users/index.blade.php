228e98439b50d6d681f3e36e963ed347f2918526
banta_andrei <andrei.banta11@gmail.com>
2021-05-11 22:07:45 +0300

    master -h2

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -120,17 +120,17 @@
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
-                        <h4 class="modal-title">Default Modal</h4>
+                        <h4 class="modal-title">Delete user</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                             <span aria-hidden="true">&times;</span>
                         </button>
                     </div>
                     <div class="modal-body">
-                        <p>One fine body&hellip;</p>
+                        <p>Are you sure you want to delete this user?</p>
                     </div>
-                    <div class="modal-footer justify-content-between">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-danger">Delete user</button>
                     </div>
                 </div>
                 <!-- /.modal-content -->
